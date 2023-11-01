b14316d27f66184745f94045f24bce1171516d98
A-P-Nesterov <anesterov6@mail.ru>
2023-11-01 16:13:57 +0300

    новые изменения в фауле JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 function printText(name, age) {
     console.log("hello my name is "+name+", Age: "+age)
 }
+let name = "pavel"
+let age = 345
+let name1 = "pavelic"
+let age1 = 1345
 printText("Petr", 15);
+printText(name, age);
 printText("miha", 55);
 printText("ira", 53);
 printText("Vitz", 25);
+printText(name1, age1);
