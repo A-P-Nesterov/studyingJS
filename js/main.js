f3b6a65c8ec6e49f78c22fd3b764cccbc4969651
A-P-Nesterov <anesterov6@mail.ru>
2023-11-01 16:08:41 +0300

    изменен файл JS

--- js/main.js
+++ js/main.js
@@ -2,3 +2,6 @@ function printText(name, age) {
     console.log("hello my name is "+name+", Age: "+age)
 }
 printText("Petr", 15);
+printText("miha", 55);
+printText("ira", 53);
+printText("Vitz", 25);
